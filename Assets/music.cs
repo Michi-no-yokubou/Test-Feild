30f9960a94bfb6b976192d1a844620386004a7cc
Gopal727727 <mandirt66@gmail.com>
2022-01-26 10:24:27 +0545

    added music

---
+++ Assets/music.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class music : MonoBehaviour
+{
+    public static music instance;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
